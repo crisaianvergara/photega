54d4f79e01de03fa07b17c8e0346e220c65e9424
Cris-aian Vergara <crisaianvergara@gmail.com>
2025-05-26 00:13:19 +0800

    Improved folder structures.

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # photega
+
+## POETRY
+- poetry env info --path
+- source <path>/bin/activate
+- to select interpreter:
+    - enter path
+    - <path>/bin/python
--- app/app.py
+++ app/app.py
@@ -1,13 +1,22 @@
+import os
+
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 
+from mangum import Mangum
+
 from .api import files
 
 app = FastAPI()
-app.mount("/static", StaticFiles(directory="static"), name="static")
-templates = Jinja2Templates(directory="templates")
+
+base_dir = os.path.dirname(__file__)
+static_path = os.path.join(base_dir, "static")
+templates_path = os.path.join(base_dir, "templates")
+
+app.mount("/static", StaticFiles(directory=static_path), name="static")
+templates = Jinja2Templates(directory=templates_path)
 
 app.include_router(files.router)
 
@@ -17,3 +26,6 @@ async def index(request: Request):
     return templates.TemplateResponse(
         request=request, name="index.html", context={}
     )
+
+
+handler = Mangum(app)
